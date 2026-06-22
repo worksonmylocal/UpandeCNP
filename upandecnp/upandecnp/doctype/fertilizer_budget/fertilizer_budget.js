1fde813087ccedf9560a6a056648e0ea792a09c6
worksonmylocal <steve@upande.com>
2026-06-22 17:59:00 +0300

    feat: add all core doctypes

---
+++ upandecnp/upandecnp/doctype/fertilizer_budget/fertilizer_budget.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Upande and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Fertilizer Budget", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ upandecnp/upandecnp/doctype/production_calendar/test_production_calendar.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Upande and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProductionCalendar(FrappeTestCase):
+	pass
